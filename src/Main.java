858acde3d0d02539b20167bb6e57658e00bb33cc
andrejstrogonov <strogonovandrej@hotmail.com>
2024-09-26 11:36:30 +0300

    Add 4 for each variable

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,11 @@ public class Main {
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
-
-        System.out.println("Hello world!");
+        dog = dog + 4.0;
+        cat = cat+4.0;
+        paper = paper+4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
     }
 }
